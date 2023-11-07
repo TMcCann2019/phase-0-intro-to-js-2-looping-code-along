8ffd0f82a9eb16591e3452d558c0271ee186dd60
TimothyMcCann <timothysmccann@gmail.com>
2023-11-07 14:28:52 -0500

    Final

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+function countDown(number){
+    let count = number
+    while (count >= 0){
+        console.log(count)
+        count = count - 1
+    }
+}
+
+function calledWithExactly(number){
+    let count = 4;
+    while (count >= 0){
+        console.log(count);
+        count = count - 1;
+    }
+}
